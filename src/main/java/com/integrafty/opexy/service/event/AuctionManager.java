04eb65bba37fd5528f54ed017b3503aaf707a0bd
Omarayman008 <omarayman008@users.noreply.github.com>
2026-05-05 02:42:35 +0300

    style: remove redundant standard emojis from auction random box

--- src/main/java/com/integrafty/opexy/service/event/AuctionManager.java
+++ src/main/java/com/integrafty/opexy/service/event/AuctionManager.java
@@ -221,14 +221,14 @@ public class AuctionManager extends ListenerAdapter {
             String prizeLower = currentPrize.toLowerCase();
             if (prizeLower.contains("صندوق عشوائي") || prizeLower.contains("صندوق غامض") || prizeLower.contains("random box") || prizeLower.contains("mystery box")) {
                 String[] randomItems = {
-                    "🏜️ <:sand:1500879926696607846> (1x Sand)", 
-                    "🪵 <:stick:1500879473992794212> (1x Stick)", 
-                    "🪨 <:coble_stone:1500879838041608243> (1x Cobblestone)", 
-                    "🪶 <:feather:1500880285431238807> (1x Feather)",
-                    "💣 <:Minecraft_Gunpowder_pngremovebgp:1500879430367707366> (1x Gunpowder)",
-                    "🕸️ <:string:1500880235510497360> (1x String)",
-                    "🐄 <:leather:1500880346206568498> (1x Leather)",
-                    "🧪 <:water_empty_bottle:1500880709315723428> (1x Glass Bottle)"
+                    "<:sand:1500879926696607846> (1x Sand)", 
+                    "<:stick:1500879473992794212> (1x Stick)", 
+                    "<:coble_stone:1500879838041608243> (1x Cobblestone)", 
+                    "<:feather:1500880285431238807> (1x Feather)",
+                    "<:Minecraft_Gunpowder_pngremovebgp:1500879430367707366> (1x Gunpowder)",
+                    "<:string:1500880235510497360> (1x String)",
+                    "<:leather:1500880346206568498> (1x Leather)",
+                    "<:water_empty_bottle:1500880709315723428> (1x Glass Bottle)"
                 };
                 displayedPrize = "📦 صندوق عشوائي ➔ (" + randomItems[new java.util.Random().nextInt(randomItems.length)] + ")";
             }
