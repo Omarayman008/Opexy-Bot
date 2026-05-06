57787b759b7328c0cde7aa36995ab1b66536887e
Omarayman008 <omarayman008@users.noreply.github.com>
2026-05-06 21:19:45 +0300

    fix: add missing ResponseEntity import in KickService

--- src/main/java/com/integrafty/opexy/service/notification/KickService.java
+++ src/main/java/com/integrafty/opexy/service/notification/KickService.java
@@ -4,6 +4,7 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
