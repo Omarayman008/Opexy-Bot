123304760c97a38483d18f68ccccba36155019d3
Omar Ayman <omaralqtany81@gmail.com>
2026-05-07 14:35:21 +0300

    Update Test.java

--- scratch/Test.java
+++ scratch/Test.java
@@ -1,8 +1,9 @@
 import net.dv8tion.jda.api.utils.messages.MessageCreateBuilder;
 public class Test {
     public static void main(String[] args) {
-        MessageCreateBuilder b = new MessageCreateBuilder();
+        MessageCreateBuilder B = new MessageCreateBuilder();
         // b.useComponentsV2(true);
     }
 }
 public class Test2 {}
+
